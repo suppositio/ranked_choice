401a1a671785bd85d7c7b08b94f7932654c4bf3e
Rostyslav Paranko <rostyslav.paranko@gmail.com>
2023-10-08 21:02:52 +0300

    Add main_func as entry point

--- ranked_choice.py
+++ ranked_choice.py
@@ -1,4 +1,8 @@
 from modules.main import Main
 
-ranked_choice_app = Main()
-ranked_choice_app.mainloop()
+def main_func():
+    ranked_choice_app = Main()
+    ranked_choice_app.mainloop()
+    
+if __name__ == "__main__":
+    main_func()    
